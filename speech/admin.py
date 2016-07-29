e8f088f628bbb484a9e33a9e7ee2875caa08afd5
Bhairav Mehta <bhairavmehta95@gmail.com>
2016-07-28 21:01:14 -0500

    admin

--- speech/admin.py
+++ speech/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Student, Enrollments, Class, Topic, Question
+
+admin.site.register(Student)
+admin.site.register(Enrollments)
+admin.site.register(Class)
+admin.site.register(Topic)
+admin.site.register(Question)
