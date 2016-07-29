e3a9f79bbde912fc84a492f737a595524f3eeb71
Bhairav Mehta <bhairavmehta95@gmail.com>
2016-07-28 20:13:56 -0500

    adding a sign up fomr

---
+++ speech/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class SignupForm(forms.Form):
+    f_name = forms.CharField(label='First Name', max_length=30)
+    l_name = forms.CharField(label='Last Name', max_length=30)
--- speech/views.py
+++ speech/views.py
@@ -3,10 +3,34 @@ from django.http import HttpResponse
 
 from .models import Greeting, Student, Enrollments, Class, Topic, Question
 
+from .forms import SignupForm
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
+    if request.method == 'POST':
+    	return render(request, 'signup.html')
+    	
     return render(request, 'index.html')
+
+def signup_user(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SignupForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            print "Thanks!"
+            return render(request, 'index.html')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'signup.html', {'form': form})
 
 
 def db(request):
