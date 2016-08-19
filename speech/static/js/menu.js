8ade377431aa929d557c146b4a805b02f086d1b3
melquick <melquick@gmail.com>
2016-08-19 11:47:24 -0500

    menu pop-out

---
+++ speech/static/js/menu.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("pop-out").style.width = "250px";
+
+document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("pop-out").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
