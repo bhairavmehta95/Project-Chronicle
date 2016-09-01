9f6e4427f15371f8eaa62b278ca5dcaf56571ca9
Reed-Anderson <reedineagan@comcast.net>
2016-08-31 21:21:35 -0500

    Header adjustments and consistency.

--- speech/static/js/menu.js
+++ speech/static/js/menu.js
@@ -1,10 +1,3 @@
-function openNav() {
-    document.getElementById("pop-out").style.width = "250px";
-
-document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("pop-out").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+function toggleNav() {
+    $('#pop-out').toggleClass('active');
 }
