04081bb1fe8b5f326cd210a4588eb14a7c74d815
Bhairav Mehta <bhairavm@umich.edu>
2017-05-27 16:42:27 -0700

    trying with no root

--- chronicle/settings.py
+++ chronicle/settings.py
@@ -131,7 +131,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
 STATIC_URL = '/static/'
 
