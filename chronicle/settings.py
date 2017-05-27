bc556f8da2e87aaf7fca15c81907358554920c38
Bhairav Mehta <bhairavm@umich.edu>
2017-05-27 16:23:51 -0700

    addin static file to global

--- chronicle/settings.py
+++ chronicle/settings.py
@@ -131,7 +131,8 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "speech", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
